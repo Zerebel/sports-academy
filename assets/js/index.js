397c53866329daafcc8afc89152a1e46393b0b56
Zerebel <kofitruth11@gmail.com>
2022-11-09 22:27:03 +0000

    cards added

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,3 +1,4 @@
+// Navbar Toggle
 const menu_items = document.querySelector('#menu_items');
 const btnMenu = document.querySelector('#menu');
 
@@ -10,3 +11,41 @@ const toggleMenu = () => {
 };
 
 btnMenu.addEventListener('click', toggleMenu, false);
+
+const debounce = (fn) => {
+  let frame;
+
+  return (...params) => {
+    if (frame) cancelAnimationFrame(frame);
+
+    frame = requestAnimationFrame(() => {
+      fn(...params);
+    });
+  };
+};
+
+// Scroll watch
+const storeScroll = () => {
+  document.documentElement.dataset.scroll = window.scrollY;
+};
+
+document.addEventListener('scroll', debounce(storeScroll), { passive: true });
+
+storeScroll();
+
+// Introduction Message
+const message = [
+  'more than 30 years in the ISF competition program',
+  'organizing sports on and off campus',
+  'empowering youth through sports and education',
+];
+// elements
+const description = document.querySelector('#description');
+let i = 0;
+const changeMessage = () => {
+  if (i > 2) i = 0;
+  description.textContent = message[i];
+  i++;
+  return;
+};
+setInterval(changeMessage, 2500);
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,18 @@
 /** @type {import('tailwindcss').Config} */
+const defaultTheme = require('tailwindcss/defaultTheme');
 module.exports = {
   content: ['./**/*.{html,js}'],
   theme: {
-    extend: {},
+    extend: {
+      backgroundImage: {
+        stadium: 'url("/assets/img/stadium.jpg")',
+      },
+      fontFamily: {
+        montserrat: ['Montserrat', ...defaultTheme.fontFamily.sans],
+        quicksand: ['Quicksand', ...defaultTheme.fontFamily.sans],
+        poppins: ['Poppins', ...defaultTheme.fontFamily.sans],
+      },
+    },
   },
   plugins: [],
 };
